9005a5423b8405f8e91e41bc164dc2694ce5d387
Corbin Crutchley <crutchcorn@gmail.com>
2020-12-18 22:23:34 -0800

    Initial work to test Vue/Angular interop

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {ChangeDetectorRef, Component} from '@angular/core';
+import {computed, Ref, ref} from '@vue/reactivity';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,42 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  constructor(private cd: ChangeDetectorRef) {
+    this.data = ref((() => {
+      const test = ref(12);
+
+      const addToPlus = (): void => {
+        test.value += 1;
+        this.setTick();
+      };
+
+      return {
+        test,
+        addToPlus
+      };
+    })());
+
+    this.setTick();
+  }
   title = 'CompositionApiDemo';
+
+  mainNumber = 0;
+  secondNumber = 0;
+
+  data: Ref<any> = ref('');
+
+  changeMainNumber: () => void = () => {};
+
+  setTick(): void {
+    const reff = this.data.value;
+
+    this.mainNumber = reff.test;
+    this.changeMainNumber = reff.addToPlus;
+  }
+
+  testValues(): void {
+    console.log('secondNumber', this.secondNumber);
+    console.log('testing', this.data);
+  }
 }
