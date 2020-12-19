f94412dbb86ad88a78129c549d660137c482e52b
Corbin Crutchley <crutchcorn@gmail.com>
2020-12-18 22:36:23 -0800

    Show demonstration that you can use decorator values inside of template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,48 +1,31 @@
-import {ChangeDetectorRef, Component} from '@angular/core';
-import {computed, Ref, ref} from '@vue/reactivity';
+/**
+ * This shows us that you can access `this.` items from the tempate inside
+ * that are exposed via a decorator
+ */
 
+import {ChangeDetectorRef, Component, OnInit} from '@angular/core';
+
+function classDecorator<T extends new (...args: any[]) => {}>(
+  constructor: T
+): { new(): ({ newProperty: string; hello: string } & any); prototype: { newProperty: string; hello: string } } {
+  return class extends constructor {
+    newProperty = 'new property';
+    hello = 'override';
+  };
+}
+
+@classDecorator
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-
+export class AppComponent implements OnInit {
+  newProperty: never;
   constructor(private cd: ChangeDetectorRef) {
-    this.data = ref((() => {
-      const test = ref(12);
-
-      const addToPlus = (): void => {
-        test.value += 1;
-        this.setTick();
-      };
-
-      return {
-        test,
-        addToPlus
-      };
-    })());
-
-    this.setTick();
-  }
-  title = 'CompositionApiDemo';
-
-  mainNumber = 0;
-  secondNumber = 0;
-
-  data: Ref<any> = ref('');
-
-  changeMainNumber: () => void = () => {};
-
-  setTick(): void {
-    const reff = this.data.value;
-
-    this.mainNumber = reff.test;
-    this.changeMainNumber = reff.addToPlus;
   }
 
-  testValues(): void {
-    console.log('secondNumber', this.secondNumber);
-    console.log('testing', this.data);
+  ngOnInit(): void {
+    console.log(this);
   }
 }
