35b8f5a4552406d036b07345d5a100435c93d807
Corbin Crutchley <crutchcorn@gmail.com>
2020-12-19 02:36:50 -0800

    Added initial props test

--- projects/vuelar/src/lib/setup.test.ts
+++ projects/vuelar/src/lib/setup.test.ts
@@ -1,14 +1,14 @@
-import {ChangeDetectorRef, Component, ComponentFactoryResolver} from '@angular/core';
+import {ChangeDetectorRef, Component, ComponentFactoryResolver, Input} from '@angular/core';
 import {render} from '@testing-library/angular';
 import {screen, waitFor} from '@testing-library/dom';
 import '@testing-library/jest-dom';
 import {Setup} from './setup';
-import {ref} from '@vue/reactivity';
+import {ref, toRef} from '@vue/reactivity';
 
 @Setup(() => {
-  const other = ref("I am other");
+  const other = ref('I am other');
   return {
-    test: "Hello, world!",
+    test: 'Hello, world!',
     other
   };
 })
@@ -44,3 +44,40 @@ it('should render absolute basic ref function return', async () => {
     expect(el).toBeInTheDocument();
   });
 });
+
+
+@Setup((props) => {
+  const other = toRef(props, 'hello');
+
+  return {
+    other
+  };
+})
+@Component({
+  selector: 'test-setup',
+  template: `
+    <p>{{other}}</p>
+  `
+})
+class PropsTestComponent {
+  other: any;
+
+  @Input() hello = '';
+
+  constructor(private componentFactoryResolver: ComponentFactoryResolver, private cd: ChangeDetectorRef) {
+  }
+}
+
+it('should render absolute basic prop ref return', async () => {
+  render(PropsTestComponent, {
+    componentProperties: {
+      hello: 'Hello, world!'
+    }
+  });
+
+  await waitFor(() => {
+    const el = screen.getByText('Hello, world!');
+    expect(el).toBeInTheDocument();
+  });
+});
+
