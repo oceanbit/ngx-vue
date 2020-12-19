12a94217d6946a320cd4bc321b88ca5a3333a8f7
Corbin Crutchley <crutchcorn@gmail.com>
2020-12-19 00:14:57 -0800

    Initial work to get pure object of props and nothing else

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,33 @@
-import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {ChangeDetectionStrategy, Component, ComponentFactoryResolver, Input} from '@angular/core';
 import {ref} from '@vue/reactivity';
 
+import 'reflect-metadata';
+import {getFilteredProperties, isFilter} from './isFilter';
+
+const metadataKey = Symbol('Input');
+
 // tslint:disable-next-line:typedef
-function Setup<SetupReturn, T extends new (...args: any[]) => any>(setupFn: (tick: () => void) => SetupReturn) {
+function Setup<SetupReturn, PropsT, T extends new (...args: any[]) => any>(setupFn: (props: PropsT, tick: () => void) => SetupReturn) {
   return (constructor: T) => {
     return class extends constructor {
       constructor(...args: any[]) {
         super(...args);
+
+        const componentFactoryResolver: ComponentFactoryResolver = args[0];
+
+        const properties = componentFactoryResolver.resolveComponentFactory(this.constructor as any);
+
+        type CompClass = keyof InstanceType<T>;
+        type CompProp = keyof CompClass;
+
+        const inputProps = properties.inputs
+          .map(input => input.propName)
+          .reduce((curr, propName) => {
+            curr[propName as CompProp] = this[propName];
+            return curr;
+          }, {} as Record<CompClass, CompClass[CompProp]>);
+
+        console.log('properties', inputProps);
 
         // tslint:disable-next-line:variable-name
         const __data = ref({});
@@ -22,7 +43,7 @@ function Setup<SetupReturn, T extends new (...args: any[]) => any>(setupFn: (tic
           // cd.detectChanges();
         };
 
-        __data.value = setupFn(detectChanges);
+        __data.value = setupFn(this, detectChanges);
 
         detectChanges();
       }
@@ -30,7 +51,7 @@ function Setup<SetupReturn, T extends new (...args: any[]) => any>(setupFn: (tic
   };
 }
 
-@Setup((detectChanges) => {
+@Setup((props: { testing: string }, detectChanges) => {
   const test = ref(12);
 
   const addToPlus = (): void => {
@@ -53,5 +74,11 @@ export class AppComponent {
   test: any;
   addToPlus: any;
 
-  constructor() {}
+  @Input() testing = '';
+
+  // TODO: Migrate to use service to DI into this:
+  // https://stackoverflow.com/a/52667101/4148154
+  constructor(private componentFactoryResolver: ComponentFactoryResolver) {
+  }
+
 }
