7592047c5bff6f92e9a58364a1a55b922e0b1185
Corbin Crutchley <crutchcorn@gmail.com>
2020-12-18 23:16:38 -0800

    Got individual keys detecting from Vue setup fn

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,41 @@
-import {Component} from '@angular/core';
+import {ChangeDetectionStrategy, Component} from '@angular/core';
 import {ref} from '@vue/reactivity';
 
 // tslint:disable-next-line:typedef
 function Setup<SetupReturn, T extends new (...args: any[]) => any>(setupFn: (tick: () => void) => SetupReturn) {
-  const tick = () => {
-  };
-
-  const data = ref(setupFn(tick));
-
   return (constructor: T) => {
     return class extends constructor {
-      data = data.value;
+      constructor(...args: any[]) {
+        super(...args);
+
+        // tslint:disable-next-line:variable-name
+        const __data = ref({});
+
+        const setVariables = (data: any) => {
+          Object.keys(data).forEach(key => {
+            this[key] = data[key];
+          });
+        };
+
+        const detectChanges = () => {
+          setVariables(__data.value);
+          // cd.detectChanges();
+        };
+
+        __data.value = setupFn(detectChanges);
+
+        detectChanges();
+      }
     };
   };
 }
 
-@Setup(() => {
+@Setup((detectChanges) => {
   const test = ref(12);
 
   const addToPlus = (): void => {
     test.value += 1;
-    // setTick();
+    detectChanges();
   };
 
   return {
@@ -31,8 +46,12 @@ function Setup<SetupReturn, T extends new (...args: any[]) => any>(setupFn: (tic
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent {
-  data: any;
+  test: any;
+  addToPlus: any;
+
+  constructor() {}
 }
